95bd1c80eab66ffc8c580f2ff59c5f087c07ad14
phase858 <phase858@gmail.com>
2018-08-28 18:18:52 -0400

    The actual stuff.

--- RexBot/Program.cs
+++ RexBot/Program.cs
@@ -1,12 +1,109 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using RexBot.CommandContent;
+using RexBot.Utilities;
 
-namespace RexBot
+namespace DiscordBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        CommandContent commands = new CommandContent();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            commands.LoadData();
+
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string token = Config.Key;
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            string prefix = Config.Prefix;
+
+            if (message.Content.Substring(0,prefix.Length) == prefix)
+            {
+                string content = message.Content.Remove(0, prefix.Length);
+                if (Utils.CommandDisabled(content.Split(new[] { ' ' }, 2)[0]))
+                {
+                    await message.Channel.SendMessageAsync("Command disabled.");
+                }
+                else
+                {
+                    if (content.Split(new[] { ' ' }, 2)[0] == "dieroll" & Config.AllowHardcoded)
+                    {
+                        string commandReturn = commands.DieRoll(message.Content.Split(new[] { ' ' }, 2)[1]);
+                        await message.Channel.SendMessageAsync(commandReturn);
+                    }
+                    
+                    else if (content.Split(new[] { ' ' }, 2)[0] == "flip" & Config.AllowHardcoded)
+                    {
+                        string commandReturn = commands.Flip(message.Content.Split(new[] { ' ' }, 2)[1]);
+                        await message.Channel.SendMessageAsync(commandReturn);
+                    }
+
+                    else if (content == "list" & Config.AllowHardcoded)
+                    {
+                        string commandReturn = commands.List();
+                        await message.Channel.SendMessageAsync(commandReturn);
+                    }
+
+                    else if (content == "reload")
+                    {
+                        if (message.Channel.Name == Config.ControlChannel)
+                        {
+                            commands.LoadData();
+                            await message.Channel.SendMessageAsync("Command files reloaded.");
+                        }
+                        else
+                        {
+                            await message.Channel.SendMessageAsync("Command not sent in bot control channel.");
+                        }                       
+                    }
+                    else
+                    {
+                        string name = content.Split(new[] { ' ' }, 2)[0];
+                        string messageContent = content;
+                        if (messageContent.Contains(" "))
+                        {
+                            messageContent = content.Split(new[] { ' ' }, 2)[1];
+                        }
+                        else
+                        {
+                            messageContent = "placeholderText";
+                        }
+                        string username = message.Author.Mention;
+                        string botname = Config.Name;;
+                        string commandReturn = commands.TryCommand(name, messageContent, username, botname);
+                        await message.Channel.SendMessageAsync(commandReturn);
+                    }
+                }
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
---
+++ RexBot/Utilities.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RexBot.Utilities
+{
+    class Conf
+    {
+        public string Key { get; set; }
+        public string[] AllowedCategories { get; set; }
+        public string[] DisallowedCategories { get; set; }
+        public bool AllowHardcoded { get; set; }
+        public bool AllowSay { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string ControlChannel { get; set; }
+        public string[] DisabledCommands { get; set; }
+    }
+    class ConfigSettings
+    {
+
+    }
+    public static class Utils
+    {
+         public static bool IsTrue(string input)
+        {
+            if (input == "true" || input == "True")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CategoryAllowed(string input)
+        {
+            bool returnValue = false;
+            foreach (string command in Config.AllowedCategories)
+            {
+                if (command == input)
+                {
+                    returnValue = true;
+                }
+            }
+            return returnValue;
+        }
+
+        public static bool CommandDisabled(string input)
+        {
+            bool match = false;
+            foreach (string command in Config.DisabledCommands)
+            {
+                if (command == input)
+                {
+                    match = true;
+                }
+            }
+            return match;
+        }
+
+        public static bool CategoryDisallowed(string input)
+        {
+            bool returnValue = false;
+            foreach (string command in Config.DisallowedCategories)
+            {
+                if (command == input)
+                {
+                    returnValue = true;
+                }
+            }
+            return returnValue;
+        }
+    }
+
+    static class Config
+    {
+        static string configFile = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "RexBot.conf"));
+
+        static Conf config = JsonConvert.DeserializeObject<Conf>(configFile);
+
+        public static string Key = config.Key;
+
+        public static string[] AllowedCategories = config.AllowedCategories;
+
+        public static string[] DisallowedCategories = config.DisallowedCategories;
+
+        public static bool AllowHardcoded = config.AllowHardcoded;
+
+        public static string Name = config.Name;
+
+        public static string Prefix = config.Prefix;
+
+        public static string ControlChannel = config.ControlChannel;
+
+        public static string[] DisabledCommands = config.DisabledCommands;
+    }
+}
